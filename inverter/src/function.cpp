747680f7dd0eec5743c4bd041587e598706abbc4
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-04-12 20:33:58 -0300

    feat: reverse vector of strings

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <array>
-
+#include <string>
 /*! 
  * Reverse de order of elements inside the array.
  * @param arr Reference to the array with the values.
@@ -8,5 +8,17 @@
 template <std::size_t SIZE>
 void reverse( std::array< std::string, SIZE > & arr )
 {
-    /*codigo*/
+    int n = arr.size();
+
+    int aux[n];
+
+    if(n > 1) {
+        for (int i = 0; i < n; i++) {
+            aux[n - 1 - i] = std::stoi(arr[i]);
+        }
+ 
+        for (int i = 0; i < n; i++) {
+            arr[i] = std::to_string(aux[i]);
+        }
+    }
 }
