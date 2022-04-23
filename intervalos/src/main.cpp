2ef2e017ae390c8bb5ae6c5bcb01589373e51310
João Paulo M <joaopaulopmedeiros@gmail.com>
2022-04-22 23:43:20 -0300

    feat: solve interval question

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -14,8 +14,34 @@ using std::setprecision;
 // Se desejar, crie funções aqui, antes do main().
 
 int main(void)
-{
-    // TODO: Adicione aqui a sua solução.
+{  
+    int x;
+    int count = 0;
+    int in1 = 0;
+    int in2 = 0;
+    int in3 = 0;
+    int in4 = 0;
+    int in5 = 0;
+    while( cin >> std::ws >> x) {
+        count++;
+        if(x >= 0 && x < 25){
+        in1++;
+        } else if(x >= 25 && x < 50){
+        in2++;
+        } else if(x >= 50 && x < 75){
+        in3++;
+        } else if(x >= 75 && x < 100){
+        in4++;
+        } else {
+        in5++;
+        }
+    }
+  
+    cout << setprecision(4) << (in1 * 100.0)/static_cast<double>(count) << endl;
+    cout << setprecision(4) << (in2 * 100.0)/count << endl;
+    cout << setprecision(4) << (in3 * 100.0)/count << endl;
+    cout << setprecision(4) << (in4 * 100.0)/count << endl;
+    cout << setprecision(4) << (in5 * 100.0)/count << endl;
 
     return 0;
 }
